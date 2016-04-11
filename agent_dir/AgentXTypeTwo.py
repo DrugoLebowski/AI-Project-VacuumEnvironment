4315f89a97a1bc8901ab1886c80d7c1037cd7aa1
Valerio Belli <belli.valerio@gmail.com>
2016-04-11 22:41:12 +0200

    * Updated agent logic

--- agent_dir/AgentXTypeTwo.py
+++ agent_dir/AgentXTypeTwo.py
@@ -27,10 +27,16 @@ class AgentXTypeTwoClass(Agent):
         # Visited floor
         self.visited_floor = []
 
+        # Search tree
+        self.search_tree = [((0, 0), 4)]
+
+        # Visited floor by an adversary
+        self.visited_floor_adv = []
+
         # Current action
         self.current_action = 4
 
-        # Position
+        # Current position
         self.position = (0, 0)
 
         # NoOp max execution
@@ -44,15 +50,15 @@ class AgentXTypeTwoClass(Agent):
                     - (tuple): The new position
             """
             if action == 0:
-                return self.position[0] - 1, self.position[1]
-            elif action == 1:
-                return self.position[0], self.position[1] - 1
-            elif action == 2:
-                return self.position[0] + 1, self.position[1]
-            elif action == 3:
                 return self.position[0], self.position[1] + 1
+            elif action == 1:
+                return self.position[0] - 1, self.position[1]
+            elif action == 2:
+                return self.position[0], self.position[1] - 1
+            elif action == 3:
+                return self.position[0] + 1, self.position[1]
 
-        def define_action():
+        def define_action(neighbors, cloned_pos, near_agent_on_X, near_agent_on_y):
             """
                 Retrieve the action to make. In first time the agent try to take open a new graph (or tree) branch,
                 if this is not possible then it enter a previously visited branch
@@ -62,37 +68,49 @@ class AgentXTypeTwoClass(Agent):
 
             """
 
-            temp_wrong_action = []
-            for i in range(0, 8):
+            for i in range(0, 5):
                 if i < 4:
-                    if get_coord(i) not in self.walls and get_coord(i) not in self.visited_floor:
+                    ##
+                    # Control if a position is not a wall, an already visited floor
+                    # and a visited position by an adversary
+
+                    if (get_coord(i) not in self.walls \
+                                and get_coord(i) not in self.visited_floor \
+                                and get_coord(i) not in self.visited_floor_adv):
+                        # New position
                         self.position = get_coord(i)
+
+                        # New action
                         self.current_action = i
+
+                        # Save in the history
+                        self.visited_floor.insert(0, self.position)
+                        self.search_tree.insert(0, (self.position, i))
                         return self.actions[i]
-                    elif get_coord(i) in self.walls:
-                        temp_wrong_action.append(i)
-                # Random
+                # Backtracking
                 else:
-                    # Calculate the random action to make
-                    action = randint(0, 5) % 5
-                    coord = get_coord(action)
-                    if action != 4:
-                        if coord not in self.walls and coord:
-                            self.position = coord
-                            self.current_action = action
-                            return self.actions[action]
-                        else:
-                            temp_wrong_action.append(action)
+                    if self.search_tree:
+                        (coord_x, coord_y), action = self.search_tree.pop(0)
                     else:
-                        self.no_op_max = self.no_op_max - 1
                         return 'NoOp'
 
-        def retrieve_most_near_agent(neighbors):
+                    # Calculate the backtrack action to make
+                    action = (action + 2) % 4
+
+                    # Backtrack position
+                    self.position = get_coord(action)
+
+                    # Backtrack action
+                    self.current_action = action
+                    return self.actions[action]
+
+        def retrieve_most_near_agent(neighbors, on):
             """
-                Retrieve the most near agent
+                Retrieve the most near agent with the indication of the axis
 
                  Args:
-                     neighbors
+                     neighbors (array)
+                     on (string)
 
                  Return:
                      (tuple)
@@ -104,8 +122,24 @@ class AgentXTypeTwoClass(Agent):
                     )
                     for (agent_id, agent_type), pos in neighbors if agent_id != self.id and agent_type == self.name
                     ],
-                key=lambda position: position[1]  # select position as filter
+                # Select axis X if on is equal 'OnX', otherwise select axis Y
+                key=lambda position: position[0] if on == 'OnX' else position[1]
             )
+
+        def retrieve_clone(neighbors):
+            """
+                Retrieve the cloned agent
+
+                Args:
+                    neighbors (array)
+
+                Return:
+                    (tuple)
+            """
+            for (agent_id, agent_type), pos in neighbors:
+                if agent_type == self.name and self.id != agent_id:
+                    return (agent_id, agent_type), pos
+            return None
 
         def retrieve_action(neighbors):
             """
@@ -119,10 +153,17 @@ class AgentXTypeTwoClass(Agent):
             """
 
             if neighbors:
-                (agent_id, agent_type), pos = retrieve_most_near_agent(neighbors)
-                return define_action(pos)
+                (near_agent_x_id, near_agent_x_type), near_agent_pos_x = retrieve_most_near_agent(neighbors, "OnX")
+                (near_agent_y_id, near_agent_y_type), near_agent_pos_y = retrieve_most_near_agent(neighbors, "OnY")
+                (cloned_id, cloned_type), cloned_pos = retrieve_clone(neighbors)
+                return define_action(
+                    neighbors,
+                    cloned_pos,
+                    near_agent_pos_x,
+                    near_agent_pos_y
+                )
             else:
-                return define_action()
+                return 'NoOp'
 
         def make_action(status, bump, neighbors):
             """
@@ -146,18 +187,24 @@ class AgentXTypeTwoClass(Agent):
                                 - 'GoEast'
                                 - 'NoOp' or 'Noop'
             """
-            if self.no_op_max == 0:
+
+            if self.position[0] == 0 and self.position[0] == 1 and self.search_tree:
                 return 'NoOp'
 
             if bump == 'Bump':
+                # Extract the position from the search tree 'cause it can't accessed anymore
+                if self.search_tree:
+                    self.search_tree.pop(0)
+
                 self.walls.append(self.position)
                 self.position = get_coord((self.current_action + 2) % 4)
-                return define_action()
+                return retrieve_action(neighbors)
             else:
+                # If the position is dirty, then suck
                 if status == 'Dirty':
                     return 'Suck'
                 else:
-                    return define_action()
+                    return retrieve_action(neighbors)
 
         def program(status, bump, neighbors):
             """Main function of the Agent.
@@ -185,9 +232,7 @@ class AgentXTypeTwoClass(Agent):
             # Save all the position visited by an other agent as personal visiting
             for (agent_id, agent_type), pos in neighbors:
                 if agent_id != self.id:
-                    self.visited_floor.append((self.position[0] + pos[0], self.position[1] + pos[1]))
-                else:
-                    self.visited_floor.append(self.position)
+                    self.visited_floor_adv.append((self.position[0] + pos[0], self.position[1] + pos[1]))
 
             return make_action(status, bump, neighbors)
 
