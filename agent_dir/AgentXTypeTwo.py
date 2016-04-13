0c60a94f6e5e42a28e6f1df25d9ca4e5bc395331
Valerio Belli <belli.valerio@gmail.com>
2016-04-13 09:58:33 +0200

    * Updated agent logic with an heuristic logic

--- agent_dir/AgentXTypeTwo.py
+++ agent_dir/AgentXTypeTwo.py
@@ -1,6 +1,5 @@
 from .agents import *
-from random import randint
-
+import scipy.spatial.distance as ds
 
 class AgentXTypeTwoClass(Agent):
     def __init__(self, x=2, y=2):
@@ -59,6 +58,15 @@ class AgentXTypeTwoClass(Agent):
                 return self.position[0] + 1, self.position[1]
 
         def get_rel_coord(action):
+            """
+                Retrieve the relative coordinate of another agent
+
+                Args:
+                    action (int): The action to undertake
+
+                Return:
+                    tuple: Coordinate
+            """
             if action == 0:
                 return 0, 1
             elif action == 1:
@@ -85,72 +93,150 @@ class AgentXTypeTwoClass(Agent):
                 Calculate the action to undertake in base of position of the other agent:
                 in particular, the move is encouraged to the other agent (not the clone)
                 Args:
-                    cloned_agent_pos (tuple)
-                    agent_on_x_pos (tuple)
-                    agent_on_y_pos (tuple)
+                    neighbors (list): The list of all the neighbors
                 Return:
                     (integer)
             """
             neg_pos = []
             for (agent_id, agent_type), pos in neighbors:
                 if self.id != agent_id:
-                    neg_pos += negated_position(pos)
+                    ##
+                    # Calculate the negated position if and only if the agent type is different respect this agent type,
+                    # and if and only if the agent position is different from the clone relative position
+                    if self.name != agent_type or (self.name == agent_type and pos[0] != 0 and pos[1] != 0):
+                        neg_pos += negated_position(pos)
             return neg_pos
+
+        def distance_from_other_agents(neighbors):
+            """
+                Calculate the distance from other agents and return the list with the preferred action to make
+
+                Args:
+                    neighbors (list): The complete list of the agent
+
+                Return:
+                    (list): A list of tuple with a structure like [(distance, [action, ...]), ...]
+            """
+            distances = []
+            for (agent_id, agent_type), pos in neighbors:
+                if self.id != agent_id:
+                    dis_from_other_agent = ds.euclidean(self.position, (self.position[0] + pos[0], self.position[1] + pos[1]))
+                    actions = []
+                    # If the type of the agent is not equal from this, then calculate the distance
+                    if agent_type != self.name:
+                        if pos[0] < 0:
+                            actions.append(1)  # GoWest
+                        elif pos[0] > 0:
+                            actions.append(3)  # GoEast
+                        elif pos[1] < 0:
+                            actions.append(2)  # GoSouth
+                        elif pos[1] > 0:
+                            actions.append(0)  # GoNorth
+                    # Otherwise, we are in the case when the agents are of the same type,
+                    # but one is the clone of the other
+                    else:
+                        if pos[0] < 0:
+                            actions.append(3)  # GoWest
+                        elif pos[0] > 0:
+                            actions.append(1)  # GoEast
+                        elif pos[1] < 0:
+                            actions.append(0)  # GoSouth
+                        elif pos[1] > 0:
+                            actions.append(2)  # GoNorth
+                        # Enter this branch iff the agent and the clone are in the same position
+                        else:
+                            actions.append(random.randint(0, 3))
+                    distances.append((dis_from_other_agent, actions))
+
+            def sorter(dis1, dis2):
+                if dis1[0] >= dis2[0]:
+                    return -1
+                else:
+                    return 1
+
+            distances.sort(sorter)
+            return distances
 
         def define_action(neighbors):
             """
                 Retrieve the action to make. In first time the agent try to take open a new graph (or tree) branch,
                 if this is not possible then it enter a previously visited branch
 
+                Args:
+                    neighbors (list): The list of the neighbors
+
                 Return:
                     (string): the action to make
-
             """
-            neg_pos = calculate_negated_position(neighbors)
-            for i in range(0, 5):
-                if i < 4:
-                    ##
-                    # Control if a position is not a wall, an already visited floor
-                    # and a visited position by an adversary
 
-                    if get_coord(i) not in self.walls and get_coord(i) not in self.visited_floor \
-                            and get_coord(i) not in self.visited_floor_adv and get_rel_coord(i) not in neg_pos:
-                        # New position
-                        self.position = get_coord(i)
+            def decide(action, neg_pos):
+                """
+                    Control if the action is possible
 
-                        # New action
-                        self.current_action = i
+                    Args:
+                        action (int): The action to undertake
+                        neg_pos (list): The position in the map that an agent cannot undertake
 
-                        # Save in the history
-                        self.visited_floor.insert(0, self.position)
-                        self.search_tree.insert(0, (self.position, i))
-                        return self.actions[i]
-                # Backtracking
+                    Return:
+                        (string) The action to make
+                        (None) If is not possible
+                """
+                coord = get_coord(action)
+                if coord not in self.walls and coord not in self.visited_floor \
+                        and coord not in self.visited_floor_adv and get_rel_coord(action) not in neg_pos:
+                    # New position
+                    self.position = coord
+
+                    # New action
+                    self.current_action = action
+
+                    # Save in the history
+                    self.visited_floor.insert(0, self.position)
+                    self.search_tree.insert(0, (self.position, action))
+                    return self.actions[action]
                 else:
-                    if self.search_tree:
-                        (coord_x, coord_y), action = self.search_tree.pop(0)
-                    else:
-                        return 'NoOp'
+                    return None
 
-                    # Calculate the backtrack action to make
-                    action = (action + 2) % 4
-                    temp_old_action = action
-                    temp_old_position = (coord_x, coord_y)
-                    if get_rel_coord(action) not in neg_pos:
-                        # Backtrack position
-                        self.position = get_coord(action)
+            neg_pos = calculate_negated_position(neighbors)
+            dis_other_agents = distance_from_other_agents(neighbors)
+            for dis, actions in dis_other_agents:
+                # Firstly try the actions calculated with heuristic
+                for i in actions:
+                    action = decide(i, neg_pos)
+                    if action:
+                        return action
 
-                        # Backtrack action
-                        self.current_action = action
-                        return self.actions[action]
-                    # Stop backtracking
-                    else:
+            # In this second step, the agent try to take one of the four action (if it's possible)
+            for i in range(0, 4):
+                action = decide(i, neg_pos)
+                if action:
+                    return action
+            ##
+            # ===========================
+            #        Backtracking
+            # ===========================
+            if not self.search_tree:
+                return 'NoOp'
 
-                        # Reinsert the position in the search path
-                        self.search_tree.insert(0, ((coord_x, coord_y), temp_old_action))
-                        self.position = temp_old_position
-                        self.current_action = temp_old_action
-                        return 'NoOp'
+            # Retrieve the position and action
+            (coord_x, coord_y), action = self.search_tree[0]
+
+            # Calculate the backtrack action to make
+            action = (action + 2) % 4
+
+            if get_coord(action) not in neg_pos:
+                # Remove the first element of search tree
+                self.search_tree.pop(0)
+
+                # Backtrack position
+                self.position = get_coord(action)
+
+                # Backtrack action
+                self.current_action = action
+                return self.actions[action]
+            # Stop backtracking
+            else:
+                return 'NoOp'
 
         def retrieve_action(neighbors):
             """
@@ -191,7 +277,7 @@ class AgentXTypeTwoClass(Agent):
                                 - 'NoOp' or 'Noop'
             """
 
-            if self.position[0] == 0 and self.position[1] == 0 and not self.search_tree:
+            if not self.search_tree:
                 return 'NoOp'
 
             if bump == 'Bump':
